cd763670ce6faf198fd7f995d46c66af913fa5de
flcrm33 <lipkin.artem89@gmail.com>
2022-04-29 10:15:36 +0300

    add keyboard

--- handlers/client.py
+++ handlers/client.py
@@ -1,11 +1,12 @@
 from aiogram import types, Dispatcher
 from create_bot import dp, bot
+from keyboards import kb_client
 
 
 # @dp.message_handler(commands=['start', 'help'])
 async def command_start(message: types.Message):
     try:
-        await bot.send_message(message.from_user.id, 'Слава Україні')
+        await bot.send_message(message.from_user.id, 'Слава Україні', reply_markup=kb_client)
         await message.delete()
     except:
         await message.reply("Спілкування з помічником через ОП, напишіть йому:\nhttps://t.me/Per_Assist_bot")
@@ -20,7 +21,8 @@ async def command_gloryofukraine(message: types.Message):
 async def command_gloryofnation(message: types.Message):
     await bot.send_message(message.from_user.id, 'Смерть ворогам')
 
-def register_handlers_client(dp : Dispatcher):
+
+def register_handlers_client(dp: Dispatcher):
     dp.register_message_handler(command_start, commands=['start', 'help'])
     dp.register_message_handler(command_gloryofukraine, commands=['Слава_Україні'])
     dp.register_message_handler(command_gloryofnation, commands=['Слава_Нації'])
