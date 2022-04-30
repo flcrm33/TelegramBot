b537ba0d2c7099fd942065a8dc3aa986c35a679a
flcrm33 <lipkin.artem89@gmail.com>
2022-04-30 15:56:33 +0300

    inline delete admin

--- handlers/admin.py
+++ handlers/admin.py
@@ -5,6 +5,7 @@ from create_bot import dp, bot
 from aiogram.dispatcher.filters import Text
 from data_base import sqlite_db
 from keyboards import admin_kb
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 ID = None
 
@@ -85,6 +86,23 @@ async def cancel_handler(message: types.Message, state: FSMContext):
     await message.reply('ОК')
 
 
+# Інлайн кнопка ВИДАЛИТИ - хендлери
+# @dp.callback_query_handler(lambda x: x.data and x.data.startswith('del '))
+async def del_callback_run(callback_query: types.CallbackQuery):
+    await sqlite_db.sql_delete_command(callback_query.data.replace('del ', ''))
+    await callback_query.answer(text=f'{callback_query.data.replace("del ", " ")} видалено.', show_alert=True)
+
+
+# @dp.message_handler(commands='Видалити')
+async def delete_item(message: types.Message):
+    if message.from_user.id == ID:
+        read = await sqlite_db.sql_read2()
+        for ret in read:
+            await bot.send_photo(message.from_user.id, ret[0], f'{ret[1]}\nОпис: {ret[2]}\nЦіна {ret[-1]}')
+            await bot.send_message(message.from_user.id, text='^^^', reply_markup=InlineKeyboardMarkup(). \
+                                   add(InlineKeyboardButton(f'Видалити {ret[1]}', callback_data=f'del {ret[1]}')))
+
+
 # Реєструємо хендлери
 def register_handlers_admin(dp: Dispatcher):
     dp.register_message_handler(cm_start, commands='Завантажити', state=None)
@@ -95,3 +113,5 @@ def register_handlers_admin(dp: Dispatcher):
     dp.register_message_handler(cancel_handler, state='*', commands='відмінити')
     dp.register_message_handler(cancel_handler, Text(equals='відмінити', ignore_case=True), state='*')
     dp.register_message_handler(make_changes_command, commands=['moderator'], is_chat_admin=True)
+    dp.register_callback_query_handler(del_callback_run, lambda x: x.data and x.data.startswith('del '))
+    dp.register_message_handler(delete_item, commands='Видалити')
